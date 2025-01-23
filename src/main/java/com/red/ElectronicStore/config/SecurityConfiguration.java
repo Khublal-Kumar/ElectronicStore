e3646ddf0b6b6388b9f899a06ace2eda5700f0c0
Khublal-Kumar <kkhublal030@gmail.com>
2025-01-23 16:21:44 +0530

    added JWT dependency

--- src/main/java/com/red/ElectronicStore/config/SecurityConfiguration.java
+++ src/main/java/com/red/ElectronicStore/config/SecurityConfiguration.java
@@ -3,13 +3,17 @@ package com.red.ElectronicStore.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -18,20 +22,37 @@ public class SecurityConfiguration {
     @Autowired
     private UserDetailsService userDetailsService;
 
+
+
     @Bean
     public PasswordEncoder passwordEncoder(){
         return  new BCryptPasswordEncoder();
     }
 
+//    @Bean
+//    public InMemoryUserDetailsManager userDetailsService() {
+//        UserDetails user = User.withUsername("admin")
+//                .password(passwordEncoder().encode("password"))
+//                .roles("ADMIN")
+//                .build();
+//
+//        UserDetails user2 = User.withUsername("user")
+//                .password(passwordEncoder().encode("password123"))
+//                .roles("USER")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(user, user2);
+//    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable()) // Disable CSRF for testing
+                .cors(cors -> cors.disable()) // Disable CORS (customize if needed)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/users/**").permitAll() // Allow public endpoints
+                        .requestMatchers("/**").permitAll() // Allow login endpoint
                         .anyRequest().authenticated() // Protect all other endpoints
                 );
-
 
         return http.build();
     }
@@ -42,6 +63,8 @@ public class SecurityConfiguration {
         daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
         return daoAuthenticationProvider;
     }
+
+
 
 
 }
