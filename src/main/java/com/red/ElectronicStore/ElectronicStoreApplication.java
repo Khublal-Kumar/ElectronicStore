9d643433b4769c7528d46e014a5711aed2046e98
Khublal-Kumar <kkhublal030@gmail.com>
2025-02-01 09:27:59 +0530

    swagger implemented

--- src/main/java/com/red/ElectronicStore/ElectronicStoreApplication.java
+++ src/main/java/com/red/ElectronicStore/ElectronicStoreApplication.java
@@ -5,6 +5,14 @@ import com.red.ElectronicStore.entities.Role;
 import com.red.ElectronicStore.entities.User;
 import com.red.ElectronicStore.repositories.RoleRepository;
 import com.red.ElectronicStore.repositories.UserRepository;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -12,10 +20,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import java.util.UUID;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Electronic Store API", version = "1.0", description = "API Documentation"))
 public class ElectronicStoreApplication {
 
 	public static void main(String[] args) {
@@ -24,6 +34,11 @@ public class ElectronicStoreApplication {
 
 	@Autowired
 	private RoleRepository roleRepository;  // Non-static
+
+
+
+
+
 
 	@Bean
 	public CommandLineRunner initializeRoles() {
--- src/main/java/com/red/ElectronicStore/config/SecurityFilterConfiguration.java
+++ src/main/java/com/red/ElectronicStore/config/SecurityFilterConfiguration.java
@@ -3,11 +3,19 @@ package com.red.ElectronicStore.config;
 import com.red.ElectronicStore.Enumeration.RoleName;
 import com.red.ElectronicStore.jwt.JwtAuthenticationEntryPoint;
 import com.red.ElectronicStore.jwt.JwtAuthenticationFilter;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -21,42 +29,53 @@ public class SecurityFilterConfiguration {
 
     private final JwtAuthenticationFilter filter;
 
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                .csrf(csrf -> csrf.disable()) // Disable CSRF for testing
+//                .cors(cors -> cors.disable()) // Disable CORS (customize if needed)
+//                .authorizeHttpRequests(auth -> auth
+//                        .requestMatchers("/swagger-ui.html").permitAll()
+//                        .requestMatchers("/v3/api-docs/**").permitAll()
+//                        .requestMatchers("/v3/api-docs").permitAll()
+//                        .requestMatchers(HttpMethod.POST,"/login").permitAll()
+//                        .requestMatchers(HttpMethod.DELETE,"/users/**").hasRole("ADMIN")
+////                        .requestMatchers(HttpMethod.DELETE, "/users/**").hasAuthority("ROLE_USER")
+//                        .anyRequest().authenticated())// Protect all other endpoints
+//                .exceptionHandling(e->e.authenticationEntryPoint(point))
+//                .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+//                .addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
+//
+//
+//        return http.build();
+//    }
+
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable()) // Disable CSRF for testing
-                .cors(cors -> cors.disable()) // Disable CORS (customize if needed)
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(HttpMethod.POST,"/login").permitAll()
-                        .requestMatchers(HttpMethod.DELETE,"/users/**").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.POST,"/users/**").hasRole("ADMIN")
-//                        .requestMatchers(HttpMethod.DELETE, "/users/**").hasAuthority("ROLE_USER")
-                        .anyRequest().authenticated())// Protect all other endpoints
-                .exceptionHandling(e->e.authenticationEntryPoint(point))
-                .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
+                .csrf(AbstractHttpConfigurer::disable) // Disable CSRF for testing
+                .cors(AbstractHttpConfigurer::disable) // Disable CORS (customize if needed)
+               .authorizeHttpRequests(auth -> auth
+                       .requestMatchers("/swagger-ui/**",
+                               "/v3/api-docs/**",
+                               "/swagger-resources/**",
+                               "/webjars/**"
+                               )
+                       .permitAll()
+                       .requestMatchers(HttpMethod.POST,"/login").permitAll()
+                       .requestMatchers(HttpMethod.DELETE,"/users/**").hasRole("ADMIN")
+                       .anyRequest().authenticated()
 
+
+               ).exceptionHandling(e->
+                        e.authenticationEntryPoint(point))
+                .sessionManagement(session->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
 
 
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        http
-//                .csrf(csrf -> csrf.disable())
-//                .authorizeHttpRequests(auth -> auth
-//                        .requestMatchers(HttpMethod.POST, "/login").permitAll()
-//                        .requestMatchers(HttpMethod.GET, "/users/image/**").permitAll() // If you want public access to images
-//                        .requestMatchers(HttpMethod.DELETE, "/users/**").hasAuthority("ROLE_ADMIN")
-//                        .requestMatchers(HttpMethod.POST, "/users/**").hasAnyAuthority("ROLE_ADMIN", "ROLE_STAFF")
-//                        .requestMatchers(HttpMethod.PUT, "/users/**").hasAnyAuthority("ROLE_ADMIN", "ROLE_USER")
-//                        .anyRequest().authenticated()
-//                )
-//                .exceptionHandling(e -> e.authenticationEntryPoint(point))
-//                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-//                .addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
-//
-//        return http.build();
-//    }
 }
